bf201464d3652ba5c00af04328435a51e953b6ce
kds <kds@laonpeople.com>
2021-08-31 15:01:26 +0900

    - add files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "
+[general]
+email = "dosp0911@gmail.com"
+" > ~/.streamlit/credentials.toml
+echo "
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+" > ~/.streamlit/config.toml
