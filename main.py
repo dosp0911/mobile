39fb55eeafe308530c6dcd89127a3af9f25b16fb
kds <kds@laonpeople.com>
2021-11-11 10:48:37 +0900

    - multi file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageOps
 import requests
 import io
 import json
@@ -19,20 +19,28 @@ if len(image_files) != 0:
 	cols = st.columns(len(image_files))
 	for i, img in enumerate(image_files):
 		pil_img = Image.open(img).convert('RGB')
+		pil_img = ImageOps.exif_transpose(pil_img)
 		cached_images[img.name] = np.array(pil_img)
-		cols[i].image(pil_img, caption=img.name, width=100)
+		cols[i].image(pil_img, caption=img.name, width=150)
 
 	choice = st.selectbox('모델 선택', models)
 	btn_start = st.button('검사')
 
 	if btn_start:
-		json_data = {"images": [{img_name: img_arr.tolist()} for img_name, img_arr in cached_images.items()]}
-		json_data["choice"] = choice
+		file_data = []
+		# file_data = {"images": [{img_name: img_arr.tolist()} for img_name, img_arr in cached_images.items()]}
+		for img in image_files:
+			file_data.append(('images', (img.name, img.getbuffer(), img.type)))
+
+		# file_data.append(("choice", choice))
 		st.subheader('결과')
-		res = requests.post(remote_server, json=json_data)
+		res = requests.post(local_server, files=file_data, data={"choice": choice})
 		# res = requests.post(local_server, json=json_data)
 		result = json.loads(res.content)
-		st.text('top3 Registration Number')
-		st.text(result["top3-reg-nums"])
-		st.text('detail info')
-		st.text(result["details"])
+		for i, r in enumerate(result):
+			st.text(f'top{i+1} 도면번호')
+			st.text(r["regNum"])
+			st.text(f'top{i+1} 품목명')
+			st.text(r["itemName"])
+			st.text(f'top{i+1} 유사도')
+			st.text(r["similarity"])
